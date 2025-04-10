30058636218344a2260c30847342672907213fe1
Marcos <marcos.sanchez@saei.mx>
2025-04-10 01:17:16 -0500

    animated number

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Marco" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Hola"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Welcome to the Banking App"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
